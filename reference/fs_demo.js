76ee0ac815f9b284558b96256b6fadf8870e04d4
jeff-gosselin <jgoz.email@gmail.com>
2019-10-27 10:19:02 -0400

    fs_module

--- reference/fs_demo.js
+++ reference/fs_demo.js
@@ -1,7 +1,27 @@
 const fs = require('fs');
 const path = require('path');
 
-fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+// Create a folder
+// fs.mkdir(path.join(__dirname, '/test'), {}, err => {
+//     if (err) throw err;
+//     console.log('Folder Created...');
+// });
+
+// Create and write file
+// fs.writeFile(path.join(__dirname, '/test', 'hello.txt'), "Hello World!", err => {
+//     if (err) throw err;
+//     console.log('File written...');
+
+//     // Inside callback we append to file
+//     fs.appendFile(path.join(__dirname, '/test', 'hello.txt'), " I love learning!", err => {
+//     if (err) throw err;
+//     console.log('File written...');
+// });
+// });
+
+// Read file
+fs.readFile(path.join(__dirname, '/test', "hello.txt"), 'utf8', (err, data) => {
     if (err) throw err;
-    console.log('Folder Created...');
+    console.log(data);
 });
+
